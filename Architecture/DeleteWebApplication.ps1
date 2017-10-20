5ad0d5fbd5531ff7ae1ac1c18c900b1d11d2197a
elucasg <elucasg@hotmail.com>
2017-10-20 09:13:08 +0200

    Update DeleteWebApplication.ps1

--- Architecture/DeleteWebApplication.ps1
+++ Architecture/DeleteWebApplication.ps1
@@ -2,6 +2,9 @@ Add-PSSnapin Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue
 
 $WebAppURL = "http://gea:2110"
 
+#Get web application information
+Get-SPWebApplication -identity $WebAppURL | Select-Object @{Expression={$_.DisplayName};Label="Name"}, @{Expression={$_.Url};Label="Url"}, @{Expression={$_.ContentDatabases[0].Name};Label="Content Database"}, @{Expression={$_.ApplicationPool.Name};Label="App Pool"}
+
 #Delete web application
 Remove-SPWebApplication -identity $WebAppURL -Confirm
 
