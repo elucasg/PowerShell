c97f6dc34f7e6deca390810cd8f63e67ea24aea1
elucasg <elucasg@hotmail.com>
2018-12-05 09:11:18 +0100

    Update sites.ps1

--- Architecture/sites.ps1
+++ Architecture/sites.ps1
@@ -1,3 +1,10 @@
+#Iterate All Site Collections
+$SPWebApp = Get-SPWebApplication $WebAppUrl
+foreach ($SPSite in $SPWebApp.Sites)
+{
+  $SPSite.Dispose()
+}
+
 #Loop Through All Sub Sites in a Site Collection
 $site = Get-SPSite $siteUrl -ErrorAction SilentlyContinue
 if ($site -ne $null)
@@ -5,5 +12,6 @@ if ($site -ne $null)
   foreach($web in $site.AllWebs)
   {
     Write-Host $web.Title
+    $web.Dispose()
   }
 }
