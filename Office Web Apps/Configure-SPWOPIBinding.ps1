b0eaef0734289fb3e081a62c7889f148fb5e75f7
elucasg <elucasg@hotmail.com>
2018-12-11 11:24:19 +0100

    Update Configure-SPWOPIBinding.ps1

--- Office Web Apps/Configure-SPWOPIBinding.ps1
+++ Office Web Apps/Configure-SPWOPIBinding.ps1
@@ -2,11 +2,6 @@
 # Disable office web apps for your entire SharePoint environment
 Remove-SPWOPIBinding -all $true
 
-# Disable the office web apps for single site collection
-$webAppsFeatureId = $(Get-SPFeature -limit all | where {$_.displayname -eq "OfficeWebApps"}).Id 
-$singleSiteCollection = Get-SPSite -Identity http://<site_name> 
-Disable-SPFeature $webAppsFeatureId -Url $singleSiteCollection.URL
-
 # Enable opening documents in client applications at Site collection level:
 #  - Log into SharePoint site >> Go to Site Settings.
 #  - Click Site collection features under Site Collection Administration section.
