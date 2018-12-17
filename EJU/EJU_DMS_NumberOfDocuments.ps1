bba3902660f6df300abff197de7ff50d415f43a8
elucasg <elucasg@hotmail.com>
2018-12-17 10:53:31 +0100

    Add files via upload

--- EJU/EJU_DMS_NumberOfDocuments.ps1
+++ EJU/EJU_DMS_NumberOfDocuments.ps1
@@ -1,9 +1,10 @@
-Add-PSSnapin Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue
+﻿Add-PSSnapin Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue
 
 #-----------------------------------------------------------------
 # Count number of documents\versions created in a year
 # 
 # PARAMETERS   webapps: comma separated list of the web applications to process
+#              sitecollections: comma separated list of site collections to process. If empty @(), all site collections processed
 #              year: year to be analized
 #              excludedLibraries: comma separated list of the document libraries not to be considered
 #
@@ -15,15 +16,16 @@ Add-PSSnapin Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue
 #-----------------------------------------------------------------
 #PARAMETERS
 
-$webapps= @("http://tpw/","http://docs/")
+$webapps = @("http://tpw/","http://docs/")
+$sitecollections = @("http://tpw/im")
 $year = 2018
 $excludedLibraries = @("Images", "Converted Forms", "Documents", "Form Templates", "List Template Gallery", "Master Page Gallery", 
                         "Web Part Gallery", "Translation Packages", "Theme Gallery", "Style Library", "Pages", "Site Collection Documents",
-                         "Site Collection Images", "Solution Gallery", "Site Assets", "wfpub", "Site Pages")
+                         "Site Collection Images", "Solution Gallery", "Site Assets", "wfpub", "Site Pages", "Migration Library")
 #-----------------------------------------------------------------
 
-$sitecollections = @()
-$lists = @()
+$sitecollectionsDocs = @()
+$listsDocs = @()
 
 foreach ($webapp in $webapps)
 {
@@ -32,72 +34,75 @@ foreach ($webapp in $webapps)
     {
         $allsites = Get-SPSite -WebApplication $webapp -Limit All
         ForEach ($Site in $allsites)
-        {                        
-            Write-Host "######### Processing site collection $($Site.RootWeb.Title) ($($Site.Url)) #########" -ForegroundColor Yellow
-            try
-            {
-                if ($Site.AllWebs -ne $null)
+        {
+            if ($sitecollections.Count -eq 0 -or $sitecollections.Contains($Site.Url)) 
+            {       
+                Write-Host "######### Processing site collection $($Site.RootWeb.Title) ($($Site.Url)) #########" -ForegroundColor Yellow
+                try
                 {
-                    ForEach ($web in $Site.AllWebs)
+                    if ($Site.AllWebs -ne $null)
                     {
-                        $TotalSiteItems = 0
-                        $TotalSiteVersions = 0
-
-                        ForEach ($List in $web.Lists)
+                        ForEach ($web in $Site.AllWebs)
                         {
-                            if (($List.BaseType -eq "DocumentLibrary") -and (-not $excludedLibraries.Contains($List.Title))) # -and $List.Title -eq "C1")
-                            {   
-                                $TotalItems = 0
-                                $TotalVersions = 0
+                            $TotalSiteItems = 0
+                            $TotalSiteVersions = 0
 
-                                Write-Host "List '$($List.Title)'" -ForegroundColor Cyan
-                                foreach ($item in $List.Items)
-                                {
-                                    $TotalItemVersions = 0
-                                    if ($item["Created"].Year -eq $year) {
-                                        $TotalItems += 1
-                                        $TotalItemVersions = -1
-                                    }
+                            ForEach ($List in $web.Lists)
+                            {
+                                if (($List.BaseType -eq "DocumentLibrary") -and (-not $excludedLibraries.Contains($List.Title)))
+                                {   
+                                    $TotalItems = 0
+                                    $TotalVersions = 0
 
-                                    foreach ($version in $item.Versions) {
-                                        if ($version.Created.Year -eq $year) {
-                                            $TotalItemVersions += 1
+                                    Write-Host "List '$($List.Title)'" -ForegroundColor Cyan
+                                    foreach ($item in $List.Items)
+                                    {
+                                        $TotalItemVersions = 0
+                                        if ($item["Created"].Year -eq $year) {
+                                            $TotalItems += 1
+                                            $TotalItemVersions = -1
                                         }
+
+                                        foreach ($version in $item.Versions) {
+                                            if ($version.Created.Year -eq $year) {
+                                                $TotalItemVersions += 1
+                                            }
+                                        }
+                                        $TotalVersions += $TotalItemVersions
                                     }
-                                    $TotalVersions += $TotalItemVersions
+
+                                    $newlistsDocsize = New-Object -TypeName PSObject -Property @{
+                                        Web = $webapp
+                                        Site = $Site.RootWeb.Title
+                                        Url = $Site.Url
+                                        List = $List.Title
+                                        Documents = $TotalItems
+                                        Versions = $TotalVersions
+                                    }
+                                    $listsDocs += $newlistsDocsize
+
+                                    $TotalSiteItems += $TotalItems
+                                    $TotalSiteVersions += $TotalVersions
+
                                 }
-
-                                $newListSize = New-Object -TypeName PSObject -Property @{
-                                    Web = $webapp
-                                    Site = $Site.RootWeb.Title
-                                    Url = $Site.Url
-                                    List = $List.Title
-                                    Documents = $TotalItems
-                                    Versions = $TotalVersions
-                                }
-                                $lists += $newListSize
-
-                                $TotalSiteItems += $TotalItems
-                                $TotalSiteVersions += $TotalVersions
-
-                            }
-                        }                        
+                            }                        
+                        }
+                        $newSize = New-Object -TypeName PSObject -Property @{
+                            Web = $webapp
+                            Site = $Site.RootWeb.Title
+                            Url = $Site.Url
+                            Documents = $TotalSiteItems
+                            Versions = $TotalSiteVersions
+                        }
+                        $sitecollectionsDocs += $newSize                                      
                     }
-                    $newSize = New-Object -TypeName PSObject -Property @{
-                        Web = $webapp
-                        Site = $Site.RootWeb.Title
-                        Url = $Site.Url
-                        Documents = $TotalSiteItems
-                        Versions = $TotalSiteVersions
-                    }
-                    $sitecollections += $newSize                                      
                 }
-            }
-            catch
-            {
-                Write-Host "Error processing site $($Site.Url)" -ForegroundColor Red
-                Write-Host $_
-                continue
+                catch
+                {
+                    Write-Host "Error processing site $($Site.Url)" -ForegroundColor Red
+                    Write-Host $_
+                    continue
+                }
             }
         }
     }
@@ -107,15 +112,15 @@ foreach ($webapp in $webapps)
     }
 }
 
-if ($sitecollections.Count -gt 0)
+if ($sitecollectionsDocs.Count -gt 0)
 {
     $directorypath = Split-Path $MyInvocation.MyCommand.Path
     $filename = "EJU_DMS_DocumentsCount_$($year).csv"
     $filenameDetailed = "EJU_DMS_DocumentsCount_$($year)_Detailed.csv"
 
-    $sitecollections | select Web, Site, Url, Documents, Versions | Export-CSV "$directorypath\$filename" -NoTypeInformation
+    $sitecollectionsDocs | select Web, Site, Url, Documents, Versions | Export-CSV "$directorypath\$filename" -NoTypeInformation
     Write-Host "Done!! File '$directorypath\$filename' generated" -ForegroundColor Green
 
-    $lists | select Web, Site, Url, List, Documents, Versions | Export-CSV "$directorypath\$filenameDetailed" -NoTypeInformation
+    $listsDocs | select Web, Site, Url, List, Documents, Versions | Export-CSV "$directorypath\$filenameDetailed" -NoTypeInformation
     Write-Host "Done!! File '$directorypath\$filenameDetailed' generated" -ForegroundColor Green
 }
